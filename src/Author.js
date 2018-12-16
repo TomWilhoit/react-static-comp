eb164692238d68283bf8b5adfc53dd7a09d208fd
Tom Wilhoit <tomwilhoit.business@gmail.com>
2018-12-16 14:12:10 -0700

    Implemented Changes

---
+++ src/Author.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button';
+import './Author.css';
+
+
+const Author = (props) => {
+return (
+<div className='author-info'>
+  <img src={props.authImg} className='author-image' />
+  <div className='author-text'>
+    <h4>{props.authName}</h4>
+    <h4>{props.estTime} read</h4>
+  </div>
+</div>
+)};
+
+export default Author;
---
+++ src/Story.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from './Button';
+import Author from './Author';
+import './Story.css';
+
+const Story = (props) => {
+  return (
+    <div className='Story'>
+      <img src={props.img} />
+      <div className='story-info'>
+        <div>
+          <h3>{props.title}</h3>
+          <p>{props.desc}</p>
+        </div>
+        <Author {...props}/>
+      </div>
+  </div>
+  )
+}
+
+export default Story;
